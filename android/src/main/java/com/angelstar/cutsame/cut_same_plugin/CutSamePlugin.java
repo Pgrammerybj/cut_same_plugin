e909d0c767b67d4365314c2d6a66d10c256f8fec
Yang.Baojiang <Yang.Baojiang@olaola.chat>
2022-10-24 10:26:20 +0800

    继续开始处理项目

--- android/src/main/java/com/angelstar/cutsame/cut_same_plugin/CutSamePlugin.java
+++ android/src/main/java/com/angelstar/cutsame/cut_same_plugin/CutSamePlugin.java
@@ -23,10 +23,6 @@ import io.flutter.plugin.common.MethodChannel.Result;
  * CutSamePlugin
  */
 public class CutSamePlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
     private Activity activity;
 
